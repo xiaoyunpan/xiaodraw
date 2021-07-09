84160be6a728b631580ff8eda9633d60daec4072
萧上晋 <84136399+Shangjin-Xiao@users.noreply.github.com>
2021-07-09 11:28:26 +0800

    Delete openkey.py

--- 画图/openkey.py
+++
@@ -1,9 +0,0 @@
-def open_key_jh():
-    f=open('_jh_.key','w')
-    f.write("eb2e0a6306fbfbaff264b5315fb17a434124e4f296b1452276932")
-    f1=open('htjh.ini','w')
-    f1.write("True")
-    f.close
-    f1.close
-    
-
