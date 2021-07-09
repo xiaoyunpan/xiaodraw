49d7b6a4833d14682274041e794cf7df0a93930b
萧上晋 <84136399+Shangjin-Xiao@users.noreply.github.com>
2021-07-09 11:30:18 +0800

    Update xgy.py

--- 画图/xgy.py
+++ 画图/xgy.py
@@ -5,8 +5,6 @@ def xgy():
                       模块:turtle,easygui
                       感谢大家的支持！
                       官方网站:https://soft.xiaoyunpan.cn""",
-            choices = ['激活','返回主菜单'])
-    if choice == '激活':
-        xjh.jh()
-    if choice == '返回主菜单':
+            choices = ['OK'])
+    if choice == 'OK':
         main.main()
