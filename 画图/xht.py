4b7a88b1decd2846beb1857a5ff84227c5944ab0
萧上晋 <84136399+Shangjin-Xiao@users.noreply.github.com>
2021-07-09 11:30:46 +0800

    Update xht.py

--- 画图/xht.py
+++ 画图/xht.py
@@ -2,7 +2,7 @@
 #本画图主程序程序由上晋开发                    *
 #上晋的软件官方网站: https://soft.xiaoyunpan.cn*
 #***********************************************
-import easygui,xht_k,main,xjh
+import easygui,xht_k,main
 def xht():
         d_c = easygui.choicebox("请选择一种操作",
                       choices = ['画弧线','画方形','自定义','返回主菜单'])
