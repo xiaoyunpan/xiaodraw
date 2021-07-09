3695cd7464b8ffc87a2ac56a58f899fb3b093309
萧上晋 <84136399+Shangjin-Xiao@users.noreply.github.com>
2021-07-09 11:54:20 +0800

    Update xht.py

--- xiaodraw/xht.py
+++ xiaodraw/xht.py
@@ -5,10 +5,12 @@
 import easygui,xht_k,main
 def xht():
         d_c = easygui.choicebox("请选择一种操作",
-                      choices = ['画弧线','画方形','自定义','返回主菜单'])
+                      choices = ['画矩形','画方形','自定义','返回主菜单'])
         if d_c == '返回主菜单':
                 main.main()
-        if d_c == '画弧线':
+        if d_c == '画矩形':
                 xht_k.hux()
+        if d_c == '画方形':
+                xht_k.fx()
         if d_c == '自定义':
                 xht_k.hx()
